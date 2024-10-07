b699c64c08c07776d9eaad6404219e7d18b55b3c
andresbonelli <andresbonellipiano@gmail.com>
2024-10-07 12:15:03 -0300

    verify user Route: basic styling and action status handling

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { verify } from "@/app/actions/verify";
+import Loader from "@/app/components/loader";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -8,11 +9,27 @@ export default function FerifyUser() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
   const email = searchParams.get("email");
+  const [isLoading, setIsLoading] = useState(true);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    if (token && email) {
-      const actionResult = verify({ token: token, email: email });
-    }
+    const verifyUser = async () => {
+      if (token && email) {
+        setIsLoading(true);
+        const actionResult = await verify({ token, email });
+
+        if (actionResult.success) {
+          setMessage(actionResult.success);
+        } else {
+          setMessage(actionResult.error || "Verification failed.");
+        }
+      } else {
+        setMessage("Missing token or email.");
+      }
+      setIsLoading(false);
+    };
+
+    verifyUser();
   }, [token, email]);
 
   return (
@@ -20,10 +37,13 @@ export default function FerifyUser() {
       <h1 className="font-MontserratBold text-xl text-center mb-5">
         Verifying User...
       </h1>
-      {token && email ? (
-        <p>We are verifying your account...</p>
+      {isLoading ? (
+        <div className="flex flex-col justify-between items-center gap-5">
+          <p>We are verifying your account...</p>
+          <Loader />
+        </div>
       ) : (
-        <p>Missing token or email.</p>
+        <p>{message}</p>
       )}
     </div>
   );
