eae61fc98bdaf499ed406030158989ec994fcb98
andresbonelli <andresbonellipiano@gmail.com>
2024-10-15 20:11:58 -0300

    refresh session logic

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -3,19 +3,50 @@ import "server-only";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
 import { UserFromDB } from "../utils/interfaces";
+import api from "@/services/api";
+import { AxiosResponse } from "axios";
 
 const secretKey = process.env.SECRET_KEY;
 const encodedKey = new TextEncoder().encode(secretKey);
 
-export async function verifySession(): Promise<UserFromDB | any> {
+async function refreshSession() {
+  const refreshCookie = cookies().get("refresh_token_cookie");
+  if (!refreshCookie) return null;
   try {
-    const cookie = cookies().get("access_token_cookie");
-    if (cookie) {
-      const { payload } = await jwtVerify(cookie?.value, encodedKey, {
+    console.log("refreshing...");
+    const res: AxiosResponse<{ access_token: string; refresh_token: string }> =
+      await api.post(
+        "/auth/refresh",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${refreshCookie.value}`,
+          },
+        }
+      );
+    if (res.status === 200) {
+      const { payload } = await jwtVerify(res.data.access_token, encodedKey, {
         algorithms: ["HS256"],
       });
       return payload["subject"];
     }
+  } catch (error) {}
+}
+
+export async function verifySession(): Promise<UserFromDB | any> {
+  const cookie = cookies().get("access_token_cookie");
+  if (!cookie) return null;
+  try {
+    const now = Date.now();
+    const { payload } = await jwtVerify(cookie.value, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    console.log("NOW", now);
+    console.log("EXP", payload.exp);
+    if (payload.exp && payload.exp < now) {
+      return await refreshSession();
+    }
+    return payload["subject"];
   } catch (error) {
     console.log("Failed to verify session");
   }
