6b6b3b299b9e6ae19849f3afb5c213bd2215ad83
andresbonelli <andresbonellipiano@gmail.com>
2024-10-15 14:12:28 -0300

    orders: redirect to /home if no session

--- src/app/(store)/orders/page.tsx
+++ src/app/(store)/orders/page.tsx
@@ -2,12 +2,17 @@ import { getUserOrders } from "@/actions/orders";
 import OrderCard from "@/app/components/cards/order_card";
 import { verifySession } from "@/lib/session";
 import { OrderFromDB } from "@/utils/interfaces";
+import { redirect } from "next/navigation";
 
 export default async function OrdersPage() {
   const session = await verifySession();
   const orders: OrderFromDB[] = session
     ? await getUserOrders(session?.id)
     : null;
+
+  if (!session) {
+    return redirect("/home");
+  }
 
   return (
     <div className="flex flex-col pt-10 gap-5">
