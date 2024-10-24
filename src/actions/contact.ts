4362a1795594f9e0d3733ff2618479308008148b
andresbonelli <andresbonellipiano@gmail.com>
2024-10-24 17:17:40 -0300

    Server Actions: contact: Error handling: missing API KEY/unkown error

--- src/actions/contact.ts
+++ src/actions/contact.ts
@@ -2,26 +2,35 @@
 
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function contact(state: any, formData: FormData) {
-  const userEmail = formData.get("email") as string;
-  const userName = formData.get("name") ?? "";
-  const subject = formData.get("subject") ?? "";
-  const message = formData.get("message") as string;
+  try {
+    const apiKey = process.env.RESEND_API_KEY;
+    if (!apiKey) {
+      throw new Error("RESEND_API_KEY is not defined");
+    }
 
-  const { data, error } = await resend.emails.send({
-    from: process.env.EMAIL_CONTACT_FROM || "info@example.com",
-    to: [process.env.EMAIL_CONTACT_TO || "info@example.com"],
-    subject: `${subject} - contact message from ${userName} <${userEmail}>`,
-    text: message,
-  });
+    const resend = new Resend(apiKey);
+    const userEmail = formData.get("email") as string;
+    const userName = formData.get("name") ?? "";
+    const subject = formData.get("subject") ?? "";
+    const message = formData.get("message") as string;
 
-  if (error) {
+    const { data, error } = await resend.emails.send({
+      from: process.env.EMAIL_CONTACT_FROM || "info@example.com",
+      to: [process.env.EMAIL_CONTACT_TO || "info@example.com"],
+      subject: `${subject} - contact message from ${userName} <${userEmail}>`,
+      text: message,
+    });
+
+    if (error) {
+      console.error({ error });
+      return { error: error.message };
+    }
+
+    console.log({ data });
+    return { success: data?.id };
+  } catch (error) {
     console.error({ error });
-    return { error: error.message };
+    return { error: error instanceof Error ? error.message : String(error) };
   }
-
-  console.log({ data });
-  return { success: data?.id };
 }
