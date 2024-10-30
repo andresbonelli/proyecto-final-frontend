943183b909f781f05a337f53a00e0fcd55e5172a
andresbonelli <andresbonellipiano@gmail.com>
2024-10-30 09:52:36 -0300

    admin products table: refactor complete "admin products" view into client component and add search bar.

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -8,27 +8,7 @@ export default async function AdminProducts() {
   const products = await getAdminProducts(session);
   return (
     <div className="flex flex-col justify-start bg-white">
-      <div className="flex flex-col justify-start p-8 gap-8">
-        <h1 className="w-full text-left font-MontserratBold text-2xl ">
-          Mis Productos
-        </h1>
-        <div className="flex flex-row justify-between items-center  ">
-          {/* <input
-            type="text"
-            placeholder="buscar..."
-            className="bg-background border p-2 text-grey rounded-md"
-          /> */}
-          <Link
-            href="/dashboard/products/create_new"
-            className="text-sm font-MontserratSemibold bg-softBlue hover:bg-blue text-white py-2 px-4 rounded-md"
-          >
-            Nuevo producto
-          </Link>
-        </div>
-      </div>
-      <div className="h-[700px] overflow-y-auto border-y">
-        <AdminProductsTable products={products ?? []} />
-      </div>
+      <AdminProductsTable products={products ?? []} />
     </div>
   );
 }
--- src/app/components/admin/products_table/index.tsx
+++ src/app/components/admin/products_table/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ProductFromDB } from "@/utils/interfaces";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { calculateDiscountPerc } from "@/utils";
 import Link from "next/link";
 
@@ -15,6 +15,8 @@ export default function AdminProductsTable({
   const [sortCriteria, setSortCriteria] =
     useState<ProductSortCriteria>("created_at");
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">(() => "desc");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(products);
 
   function handleSort(criteria: ProductSortCriteria) {
     if (sortCriteria === criteria) {
@@ -24,8 +26,15 @@ export default function AdminProductsTable({
       setSortOrder("asc");
     }
   }
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm, products]);
 
-  const sortedProducts = [...products].sort((a, b) => {
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
     if (sortCriteria) {
       const aValue = a[sortCriteria] ?? 0; // Default to empty string if undefined
       const bValue = b[sortCriteria] ?? 0;
@@ -66,107 +75,142 @@ export default function AdminProductsTable({
   });
 
   return (
-    <table>
-      <thead>
-        <tr className="bg-background border-b text-xs text-gray-500 h-12">
-          <th className="p-2 cursor-pointer" onClick={() => handleSort("name")}>
-            NOMBRE{" "}
-            {sortCriteria === "name" && (sortOrder === "asc" ? "▲" : "▼")}
-          </th>
-          <th
-            className="p-2 cursor-pointer"
-            onClick={() => handleSort("category")}
+    <>
+      <div className="flex flex-col justify-start p-8 gap-8">
+        <h1 className="w-full text-left font-MontserratBold text-2xl ">
+          Mis Productos
+        </h1>
+        <div className="flex flex-row justify-between items-center  ">
+          <input
+            type="text"
+            placeholder="buscar..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="bg-background border p-2 text-grey rounded-md"
+          />
+          <Link
+            href="/dashboard/products/create_new"
+            className="text-sm font-MontserratSemibold bg-softBlue hover:bg-blue text-white py-2 px-4 rounded-md"
           >
-            CATEGORIA{" "}
-            {sortCriteria === "category" && (sortOrder === "asc" ? "▲" : "▼")}
-          </th>
-          <th className="p-2">DESCRIPCION</th>
-          <th
-            className="p-2 cursor-pointer"
-            onClick={() => handleSort("price")}
-          >
-            PRECIO{" "}
-            {sortCriteria === "price" && (sortOrder === "asc" ? "▲" : "▼")}
-          </th>
-          <th className="p-2" onClick={() => handleSort("old_price")}>
-            DESC.
-            {sortCriteria === "old_price" && (sortOrder === "asc" ? "▲" : "▼")}
-          </th>
-          <th className="p-2 cursor-pointer" onClick={() => handleSort("sku")}>
-            SKU {sortCriteria === "sku" && (sortOrder === "asc" ? "▲" : "▼")}
-          </th>
-
-          <th
-            className=" p-2 cursor-pointer"
-            onClick={() => handleSort("stock")}
-          >
-            STOCK{" "}
-            <span>
-              {sortCriteria === "stock" && (sortOrder === "asc" ? "▲" : "▼")}
-            </span>
-          </th>
-          <th
-            className="p-2 cursor-pointer"
-            onClick={() => handleSort("sales_count")}
-          >
-            VENTAS{" "}
-            <span>
-              {sortCriteria === "sales_count" &&
-                (sortOrder === "asc" ? "▲" : "▼")}
-            </span>
-          </th>
-
-          <th className="p-2 ">ACCIONES</th>
-        </tr>
-      </thead>
-      {sortedProducts && (
-        <tbody>
-          {sortedProducts.map((product) => {
-            return (
-              <tr
-                key={product.id}
-                className="bg-white hover:bg-background border-y text-md"
+            Nuevo producto
+          </Link>
+        </div>
+      </div>
+      <div className="h-[700px] overflow-y-auto border-y">
+        <table>
+          <thead>
+            <tr className="bg-background border-b text-xs text-gray-500 h-12">
+              <th
+                className="p-2 cursor-pointer"
+                onClick={() => handleSort("name")}
               >
-                <td className="p-4 font-MontserratSemibold">{product.name}</td>
-                <td className="p-4 font-MontserratSemibold">
-                  {product.category ?? "—"}
-                </td>
-                <td className="p-4 text-gray-500">
-                  {product.description ?? "Sin descripción"}
-                </td>
-                <td className="p-4 font-MontserratSemibold">
-                  ${product.price}
-                </td>
-                <td className="p-4 font-MontserratSemibold">
-                  {product.old_price
-                    ? `${calculateDiscountPerc(
-                        product.old_price,
-                        product.price
-                      )}%`
-                    : "—"}
-                </td>
-                <td className="p-4">{product.sku ?? "—"}</td>
-                <td className="p-4">{product.stock}</td>
-                <td className="p-4">{product.sales_count ?? "—"}</td>
-                <td className="p-4 flex flex-row items-center gap-2">
-                  <Link
-                    href={`/dashboard/products/edit/${product.id}`}
-                    className="text-sm font-MontserratSemibold bg-softBlue hover:bg-blue text-white p-2  rounded-md"
+                NOMBRE{" "}
+                {sortCriteria === "name" && (sortOrder === "asc" ? "▲" : "▼")}
+              </th>
+              <th
+                className="p-2 cursor-pointer"
+                onClick={() => handleSort("category")}
+              >
+                CATEGORIA{" "}
+                {sortCriteria === "category" &&
+                  (sortOrder === "asc" ? "▲" : "▼")}
+              </th>
+              <th className="p-2">DESCRIPCION</th>
+              <th
+                className="p-2 cursor-pointer"
+                onClick={() => handleSort("price")}
+              >
+                PRECIO{" "}
+                {sortCriteria === "price" && (sortOrder === "asc" ? "▲" : "▼")}
+              </th>
+              <th className="p-2" onClick={() => handleSort("old_price")}>
+                DESC.
+                {sortCriteria === "old_price" &&
+                  (sortOrder === "asc" ? "▲" : "▼")}
+              </th>
+              <th
+                className="p-2 cursor-pointer"
+                onClick={() => handleSort("sku")}
+              >
+                SKU{" "}
+                {sortCriteria === "sku" && (sortOrder === "asc" ? "▲" : "▼")}
+              </th>
+
+              <th
+                className=" p-2 cursor-pointer"
+                onClick={() => handleSort("stock")}
+              >
+                STOCK{" "}
+                <span>
+                  {sortCriteria === "stock" &&
+                    (sortOrder === "asc" ? "▲" : "▼")}
+                </span>
+              </th>
+              <th
+                className="p-2 cursor-pointer"
+                onClick={() => handleSort("sales_count")}
+              >
+                VENTAS{" "}
+                <span>
+                  {sortCriteria === "sales_count" &&
+                    (sortOrder === "asc" ? "▲" : "▼")}
+                </span>
+              </th>
+
+              <th className="p-2 ">ACCIONES</th>
+            </tr>
+          </thead>
+          {sortedProducts && (
+            <tbody>
+              {sortedProducts.map((product) => {
+                return (
+                  <tr
+                    key={product.id}
+                    className="bg-white hover:bg-background border-y text-md"
                   >
-                    Editar
-                  </Link>
-                  <Link
-                    href={`/dashboard/products/delete/${product.id}`}
-                    className="text-sm font-MontserratSemibold bg-red hover:bg-redder text-white p-2 rounded-md"
-                  >
-                    Eliminar
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      )}
-    </table>
+                    <td className="p-4 font-MontserratSemibold">
+                      {product.name}
+                    </td>
+                    <td className="p-4 font-MontserratSemibold">
+                      {product.category ?? "—"}
+                    </td>
+                    <td className="p-4 text-gray-500">
+                      {product.description ?? "Sin descripción"}
+                    </td>
+                    <td className="p-4 font-MontserratSemibold">
+                      ${product.price}
+                    </td>
+                    <td className="p-4 font-MontserratSemibold">
+                      {product.old_price
+                        ? `${calculateDiscountPerc(
+                            product.old_price,
+                            product.price
+                          )}%`
+                        : "—"}
+                    </td>
+                    <td className="p-4">{product.sku ?? "—"}</td>
+                    <td className="p-4">{product.stock}</td>
+                    <td className="p-4">{product.sales_count ?? "—"}</td>
+                    <td className="p-4 flex flex-row items-center gap-2">
+                      <Link
+                        href={`/dashboard/products/edit/${product.id}`}
+                        className="text-sm font-MontserratSemibold bg-softBlue hover:bg-blue text-white p-2  rounded-md"
+                      >
+                        Editar
+                      </Link>
+                      <Link
+                        href={`/dashboard/products/delete/${product.id}`}
+                        className="text-sm font-MontserratSemibold bg-red hover:bg-redder text-white p-2 rounded-md"
+                      >
+                        Eliminar
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </>
   );
 }
